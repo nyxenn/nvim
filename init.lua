1ba363d0e72354c3ee3914e35fbd245121290e63
Nyxenn <jordib.edu@gmail.com>
2024-05-22 21:04:59 +0200

    Prettier as default for more langs

--- init.lua
+++ init.lua
@@ -628,6 +628,9 @@ require('lazy').setup({
                 -- You can use a sub-list to tell conform to run *until* a formatter
                 -- is found.
                 javascript = { { 'prettierd', 'prettier' } },
+                html = { { 'prettierd', 'prettier' } },
+                typescript = { { 'prettierd', 'prettier' } },
+                json = { { 'prettierd', 'prettier' } },
             },
         },
     },
